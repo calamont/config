b5d80446767ab2433216da0925d4cfd3673db48a
Callum Lamont <callum.lamont@complyadvantage.com>
2024-03-08 08:08:08 +0000

    add jvm ftplugins

---
+++ .config/nvim/after/ftplugin/kotlin.lua
@@ -0,0 +1,7 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true                   -- tabs are spaces
+vim.opt_local.tabstop = 4                        -- number of visual spaces per TAB
+vim.opt_local.softtabstop = 4                    -- number of spaces in tab when editing
+vim.opt_local.shiftround = true
+vim.opt_local.scrolloff = 4                      -- start scrolling X lines from screen top/bottom
+vim.opt.wrap = false                             -- don't wrap text around
